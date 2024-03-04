50cf6014c1381e4eec263f28c2ef8e64ef548f9b
AmirhosseinDelshadi <AmirhosseinDelshadi@users.noreply.github.com>
2024-03-04 09:29:37 -0500

    background scale fix

--- fr/main.js
+++ fr/main.js
@@ -10,3 +10,8 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
+
+    // No page function
+    function NoPage() {
+        alert("Cette page n'existe pas ou est en cours de creation!");
+    }
