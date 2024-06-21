9a0b2c151f465a76f7f504d90fe7560c4b039ffd
AmirhosseinDelshadi <113480676+AmirhosseinDelshadi@users.noreply.github.com>
2024-06-21 19:47:39 +0000

    update and 404

--- js/en.js
+++ js/en.js
@@ -1,16 +1,3 @@
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
     // No page function
     function NoPage() {
         alert("This page does not exist or is being created!");
